f4f013c11c637cfac1607607d59f4a5b3d857e39
Bodnár Attila <sitebuilder.body@gmail.com>
2013-05-01 22:36:54 +0300

    Update logo.js

--- logo.js
+++ logo.js
@@ -1,5 +1,11 @@
-$(document).ready(function(){$("a.anim_logo").hover(function(){
-  $(this).parent(".brand-log").children(".mouseover").children(".log").css
+/**
+ * js logo animation effects
+ */
+ 
+$(document).ready(function() {
+ // animations that occur between opening and closing 
+ $("a.anim_logo").hover(function() {
+   $(this).parent(".brand-log").children(".mouseover").children(".log").css
   ({animation:"animElogOut 0.5s ease both","-moz-animation":"animElogout 0.5s ease both"
     
   
