d5461e4993cb7a8e3cb76baa10b433dc5092b936
Bodesz <sitebuilder.body@gmail.com>
2013-05-01 15:10:20 +0300

    Update logo.js

--- logo.js
+++ logo.js
@@ -1,1 +1,5 @@
-$(document).ready(function(){$("a.anim_logo").hover(function(){$(this).parent(".logan").children
+$(document).ready(function(){$("a.anim_logo").hover(function(){
+  $(this).parent(".brand-log").children(".mouseover").children(".log").css
+  ({animation:"animElogOut 0.5s ease both","-moz-animation":"animElogout 0.5s ease both"
+    
+  
