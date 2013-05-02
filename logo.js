18263b0320c40ff09495a7dbdcf56280667cdbaa
Bodnár Attila <sitebuilder.body@gmail.com>
2013-05-02 11:22:49 +0300

    Update logo.js

--- logo.js
+++ logo.js
@@ -2,10 +2,32 @@
  * js logo animation effects
  */
  
-$(document).ready(function() {
- // animations that occur between opening and closing 
- $("a.anim_logo").hover(function() {
-   $(this).parent(".brand-log").children(".mouseover").children(".log").css
-  ({animation:"animElogOut 0.5s ease both","-moz-animation":"animElogout 0.5s ease both"
-    
-  
+$(document).ready(function () {
+    $("a.anim-logo").hover(function () {
+        $(this).parent(".brand-log").children(".mouseover").children(".log").css({
+            animation: "animElogOut 0.8s ease both",
+            "-moz-animation": "tranSloganOut 0.8s ease both",
+            "-webkit-animation": "tranSloganOut 0.8s ease both",
+            "-o-animation": "tranSloganOut 0.8s ease both"
+        });
+        $(this).parent(".brand-log").children(".mouseover").children(".blackhead").css({
+            animation: "animElogOut 0.8s ease both",
+            "-moz-animation": "animElog 0.8s ease both",
+            "-webkit-animation": "animElog 0.8s ease both",
+            "-o-animation": "animElog 0.8s ease both"
+        })
+    }, function () {
+        $(this).parent(".brand-log").children(".mouseover").children(".log").css({
+            animation: "animElogOut 0.8s ease both",
+            "-moz-animation": "animElog 0.8s ease both",
+            "-webkit-animation": "animElog 0.8s ease both",
+            "-o-animation": "animElog 0.8s ease both"
+        });
+        $(this).parent(".brand-log").children(".mouseover").children(".blackhead").css({
+            animation: "animElogOut 0.8s ease both",
+            "-moz-animation": "animElog 0.8s ease both",
+            "-webkit-animation": "animElog 0.8s ease both",
+            "-o-animation": "animElog 0.8s ease both"
+        })
+    })
+})
